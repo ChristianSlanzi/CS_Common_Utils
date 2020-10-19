e00e72af488cc63ae6c3be62706956a538443f51
Christian Slanzi <christian.slanzi@gmail.com>
2020-10-19 17:10:03 +0200

    first commit

---
+++ Example/CS_Common_Utils/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CS_Common_Utils
+//
+//  Created by ChristianSlanzi on 10/14/2020.
+//  Copyright (c) 2020 ChristianSlanzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
